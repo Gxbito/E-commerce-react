3f3fe0932008d81307b935c7b781ed5a09c06acc
Gaby Matinez <g4bmz02@gmail.com>
2023-12-21 16:04:37 -0300

    first commit

---
+++ E-commerce Lite/src/styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+ :root {
+    --main-color: white;
+    --text-color: #706E6E;
+    --secondary-color: #FF4747;
+ }
+
+ html {
+   scroll-behavior: smooth;
+ }
+
+
+`;
